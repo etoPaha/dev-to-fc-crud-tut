2fae457c87631752369e390bf627223f3db05555
Pavel Berezkin <cheliabkns@gmail.com>
2023-07-01 13:57:23 +1000

    Добавил Dockerfile

--- crud-api/Program.cs
+++ crud-api/Program.cs
@@ -18,16 +18,13 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(defConn
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapDefaultControllerRoute();
 
 app.Run();
